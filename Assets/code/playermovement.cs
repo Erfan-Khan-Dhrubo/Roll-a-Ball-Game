8eb12135a3cce1beac68c25ca1d4d1f92484d369
Erfan Khan <erfan.khan.cse@gmail.com>
2025-04-17 13:59:26 +0600

    Adding the project

---
+++ Assets/code/playermovement.cs
@@ -0,0 +1,47 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playermovement : MonoBehaviour
+{
+    
+    public float speed;
+    public TMP_Text countText;
+    public TMP_Text winText;
+    
+    private Rigidbody _rb;
+    private int _count;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _count = 0;
+        countText.text = "Count: " + _count.ToString();
+        winText.text = "";
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        _rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("pickup"))
+        {
+            other.gameObject.SetActive(false);
+            _count++;
+            countText.text = "Count: " + _count.ToString();
+            if (_count >= 12)
+            {
+                winText.text = "You Win!";
+            }
+        }
+    }
+}
